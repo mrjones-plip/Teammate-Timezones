47d164386eed5c545147903a19848cc65d7234e7
mrjones-plip <mrjones-plip@plip.com>
2021-05-12 23:04:01 -0700

    update map, fix sample script, update .gitignore

--- runSamples.sh
+++ runSamples.sh
@@ -2,6 +2,7 @@
 
 cp lookups/country.city.timezone.offset_dist.csv lookups/country.city.timezone.offset.csv
 cp lookups/sub-cleanse_dist.sh lookups/sub-cleanse.sh
+chmod +x lookups/sub-cleanse.sh
 cp input/Team-A.csv-dist input/Team-A.csv
 cp input/Team-B.csv-dist input/Team-B.csv
 
