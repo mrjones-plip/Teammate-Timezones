5b6c3546cb8a6c2d9fc04eef21c3fa26de38ba2d
mrjones-plip <mrjones-plip@plip.com>
2021-05-20 12:52:08 -0700

    finish port, update index.html to accomidate new fields, append entries to worldcities

--- clean.py
+++ clean.py
@@ -1,88 +1,77 @@
 
 
-import re, csv, glob
+import csv
+import glob
 
 csvFiles = glob.glob('./input/*.csv')
+
 citiesFile = open("./lookups/worldcities.csv")
-# countryData = countryFile.read();
+timezonesFile = open("./lookups/country.city.timezone.offset.csv")
+outputFile = open('./html/outputFinal.csv', 'w', newline='')
+
 cities = csv.reader(citiesFile)
+timezones = csv.reader(timezonesFile)
+output = csv.writer(outputFile)
 
-#   0       1           2      3    4         5     6       7             8         9           10
-# "city","city_ascii","lat","lng","country","iso2","iso3","admin_name","capital","population","id"
-
+header = ["Teammate", "Team", "Title", "City", "Country",
+          "Hub", "latitude", "longitude", "iso_a2", "timezone"]
+output.writerow(header)
 
 for file in csvFiles:
     reader = csv.reader(open(file, "r"))
+    isFirstRow = True
     for teammate in reader:
 
-        foundCity = 'nope'
-        for city in cities:
+        # first for has column headers, skip it
+        if isFirstRow is True:
+            isFirstRow = False
+            continue
 
-            foundCity = 'nope'
-            # if teammate[4] == city[4] and teammate[3] == city[0]:
-            # print(teammate[3] + ' match ' + city[0])
-            if teammate[3] == city[0]:
-                foundCity = str(city)
-                break
+        # set local vars for city and country. default found to string 'nope'
+        teammateCity = teammate[3]
+        teammateCountry = teammate[4]
+
+        # rewind citiesFile CSV to beginning. if this isn't here "for city in cities" does nothing on N + 1 run
         citiesFile.seek(0)
-            # else:
-            #     foundCity = 'nope :('
-        #     for col in row:
-        #         print(col)
+        timezonesFile.seek(0)
 
-        if foundCity == 'nope':
-            print(teammate[3] + ' ' + teammate[4] + ': ' + foundCity)
+        # todo: cache found cities so do avoid this loop on cities we've seen before
+        # todo: handle soft matches/more than one match - city names are NOT unique!
+        foundCity = False
+        for city in cities:
+            searchCity = city[0]
+            searchCityAdmin = city[7]
+            searchCountry = city[4]
+            searchISO2 = city[5]
+            searchISO3 = city[6]
+            lat = city[2]
+            lon = city[3]
+            if (teammateCountry == searchCountry or teammateCountry == searchISO3) and \
+                    (teammateCity == searchCity or teammateCity == searchCityAdmin):
+                foundCity = True
+                teammate.append(lat)
+                teammate.append(lon)
+                teammate.append(searchISO2)
+                break
 
+        if not foundCity:
+            teammate.append('NA')
+            teammate.append('NA')
+            teammate.append('NA')
+            print('Could not resolve this teammate to a lat/lon: ' + str(teammate))
 
-# #!/bin/bash
-#
-# # clean up from prior runs
-# cat /dev/null > output.csv
-# cat /dev/null > ./html/outputFinal.csv
-#
-# # clean up files and concat to one single file
-# for file in ./input/*.csv; do
-#     if [ -f "$file" ]; then
-#         tail -n +2 $file >> output.csv
-#         echo -e "\n" >> output.csv
-#     fi
-# done
-#
-# if [[ -f "./lookups/sub-cleanse.sh" && -x "./lookups/sub-cleanse.sh" ]]
-# then
-#     ./lookups/sub-cleanse.sh
-# else
-#   echo "Skipping './lookups/sub-cleanse.sh' - not found or not executable";
-# fi
-#
-# # download
-# # https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.73.zip
-# # extract worldcities.csv into ./lookups/
-# #
-# # create file with unique ISO,country,city,timezone from
-# # official timezone list: https://en.wikipedia.org/wiki/Zone.tab
-#
-# # inject lat lon and timezone
-# echo "Teammate,Team,Title,City,Country,ISO,Hub,latitude,longitude,timezone" > ./html/outputFinal.csv
-# while read p; do
-#   if [ "$p" != "" ]; then
-#     countryISO=$( echo "$p"|cut -d"," -f2 )
-#     countryFUll=$( echo "$p"|cut -d"," -f3 )
-#     city=$( echo "$p"|cut -d"," -f4 )
-#     latLonLine=$( grep "^\"$city" ./lookups/worldcities.csv | grep "\"$countryISO\"" | head -1 )
-#
-#     if [[ -f "./lookups/country.city.timezone.offset.csv"  ]]
-#     then
-#       timezoneLine=$( grep "^$countryISO,$countryFUll,$city" ./lookups/country.city.timezone.offset.csv )
-#     else
-#       timezoneLine='NA'
-#     fi
-#
-#     lat=$( echo "$latLonLine"|cut -d"\"" -f6)
-#     lon=$( echo "$latLonLine"|cut -d"\"" -f8 )
-#     tz=$( echo "$timezoneLine"|cut -d"," -f4 )
-#     echo "$p,$lat,$lon,$tz" >> ./html/outputFinal.csv
-#   fi
-# done < output.csv
-#
-# rm output.csv
+        foundTimezone = False
+        for timezone in timezones:
+            searchCity = timezone[2]
+            searchISO2 = timezone[0]
+            searchTimezone = timezone[3]
+            if teammateCity == searchCity:
+                foundTimezone = True
+                teammate.append(searchTimezone)
+                break
+
+        if not foundTimezone:
+            teammate.append('NA')
+            print('Could not resolve this teammate to a timezone: ' + str(teammate))
+
+        output.writerow(teammate)
